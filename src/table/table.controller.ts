fcebcaf134128a5d25ee95e439438cbd8878ca8b
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-22 14:26:42 -0300

    feat(table): implement `delete`

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateTableDto } from './dto/create-table.dto';
 import { UpdateTableDto } from './dto/update-table.dto';
@@ -40,5 +50,14 @@ export class TableController {
   })
   update(@Param('id') id: string, @Body() dto: UpdateTableDto): Promise<Table> {
     return this.tableService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Remover uma mesa pelo ID',
+  })
+  delete(@Param('id') id: string) {
+    this.tableService.delete(id);
   }
 }
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -30,4 +30,8 @@ export class TableService {
       data,
     });
   }
+
+  async delete(id: string) {
+    await this.prisma.table.delete({ where: { id } });
+  }
 }
