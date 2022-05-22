d2d15505d127ef2c63f505646dafc991f7691a76
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-22 14:09:41 -0300

    feat(table): implement `update`

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTableDto } from './dto/create-table.dto';
+import { UpdateTableDto } from './dto/update-table.dto';
 import { Table } from './entities/table.entity';
 
 @Injectable()
@@ -19,5 +20,14 @@ export class TableService {
     const data: Table = { ...dto };
 
     return this.prisma.table.create({ data });
+  }
+
+  update(id: string, dto: UpdateTableDto): Promise<Table> {
+    const data: Partial<Table> = { ...dto };
+
+    return this.prisma.table.update({
+      where: { id },
+      data,
+    });
   }
 }
