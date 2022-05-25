f26dc3ee9a28a7791b855daa1d179098827b2385
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-24 22:59:01 -0300

    feat(product): implement dto validation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do produto',
+    example: 'Pizza de Mussarela',
+  })
   name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Descrição do produto',
+    example:
+      'Queijo Mussarela fino do fino, massa fina e borda recheada de catupiry',
+  })
   description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'Preço do produto',
+    example: 16.48,
+  })
   price: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do produto',
+    example: 'https://i.imgur.com/hNE75Iw.png',
+  })
   image: string;
 }
