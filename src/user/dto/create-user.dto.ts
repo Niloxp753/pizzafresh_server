5aa98e1fe3c11bfab591f461aaa0c8eb996d1c08
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-24 23:28:34 -0300

    feat(user): implement dto & entity

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,42 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl, Matches, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome de usuário. Utilizado no login. Deve ser único',
+    example: 'niloxp',
+  })
+  nickname: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do usuário. Apenas para exibição',
+    example: 'Danilo Diniz',
+  })
+  name: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca',
+  })
+  @ApiProperty({
+    description: 'Senha do usuário para login',
+    example: 'Abcd@1234',
+  })
+  password: string;
+
+  @ApiProperty({
+    description: 'A confirmação da senha deve ser igual a senha',
+    example: 'Abcd@1234',
+  })
+  confirmPassword: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem de perfil do usuário',
+    example: 'https://avatars.githubusercontent.com/u/97798047',
+  })
+  image: string;
+}
